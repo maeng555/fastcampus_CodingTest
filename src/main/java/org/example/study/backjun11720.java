a0e5dbe44edeedd540db93319b7c3bbe436329d3
maeng555 <mjy5188@naver.com>
2025-06-17 15:12:55 +0900

    숫자의 합

--- src/main/java/org/example/study/backjun11720.java
+++ src/main/java/org/example/study/backjun11720.java
@@ -1,19 +1,17 @@
-package org.example;
+package org.example.study;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class backjun11720 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int cnt = a;
         String b = sc.next();
         char[] arr = b.toCharArray();
 
         int sum = 0;
-        for (int i = 0; i < cnt; i++) {
+        for (int i = 0; i < a; i++) {
             sum += (char) Integer.parseInt(String.valueOf(arr[i]));
         }
         System.out.println(sum);
@@ -21,3 +19,4 @@ public class Main {
 
     }
 }
+
