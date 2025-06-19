9b0210c218eac4c36913c0ea88315c33116f03ba
maeng555 <mjy5188@naver.com>
2025-06-20 02:01:27 +0900

    1427 - 선택정렬로 (sort없이 풀기)

--- src/main/java/org/example/study/baekjoon1427.java
+++ src/main/java/org/example/study/baekjoon1427.java
@@ -20,3 +20,40 @@ public class baekjoon1427 {
 
     }
 }
+//선택정렬로 풀기
+//package org.example.study;
+//
+//import java.util.Scanner;
+//
+//public class baekjoon1427 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String input = sc.next();
+//        int[] digits = new int[input.length()];
+//
+//        // 1. 문자열 → 정수 배열로 변환
+//        for (int i = 0; i < input.length(); i++) {
+//            digits[i] = input.charAt(i) - '0'; // '3' → 3
+//        }
+//
+//        // 2. 선택 정렬 (내림차순)
+//        for (int i = 0; i < digits.length - 1; i++) {
+//            int maxIdx = i;
+//            for (int j = i + 1; j < digits.length; j++) {
+//                if (digits[j] > digits[maxIdx]) {
+//                    maxIdx = j;
+//                }
+//            }
+//
+//            // swap
+//            int temp = digits[i];
+//            digits[i] = digits[maxIdx];
+//            digits[maxIdx] = temp;
+//        }
+//
+//        // 3. 출력
+//        for (int digit : digits) {
+//            System.out.print(digit);
+//        }
+//    }
+//}
