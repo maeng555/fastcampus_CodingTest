8589c45ca0a664c84b68f5b35213a1e457aff38c
maeng555 <mjy5188@naver.com>
2025-06-29 20:52:57 +0900

    baekjoon 2444 - repeat version

--- src/main/java/org/example/study/baekjoon2444.java
+++ src/main/java/org/example/study/baekjoon2444.java
@@ -1,7 +1,7 @@
 package org.example.study;
 
 import java.util.Scanner;
-
+//repeat을 써서 시간복잡도 n을 이용해서 풀수잇음
 public class baekjoon2444 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -26,5 +26,21 @@ public class baekjoon2444 {
 
             System.out.println();
         }
+        //repeat version
+
+//                Scanner sc = new Scanner(System.in);
+//                int a = sc.nextInt();
+//
+//                for (int i = 1; i <= a; i++) {
+//                    System.out.print(" ".repeat(a - i));
+//                    System.out.println("*".repeat(2 * i - 1));
+//                }
+//
+//                for (int i = a - 1; i >= 1; i--) {
+//                    System.out.print(" ".repeat(a - i));
+//                    System.out.println("*".repeat(2 * i - 1));
+//                }
+
+        }
     }
-}
+
